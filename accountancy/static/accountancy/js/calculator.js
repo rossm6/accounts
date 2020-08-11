f11113e0eb8506a4b5a97bc663c9394abd321dc0
Ross Meredith <rossm6@googlemail.com>
2020-08-11 23:06:42 +0100

    added the outstanding and matched values to the UI

--- accountancy/static/accountancy/js/calculator.js
+++ accountancy/static/accountancy/js/calculator.js
@@ -26,6 +26,7 @@ $(document).ready(function () {
     }
 
     function calculate_totals() {
+        // first calculate the totals for goods, vat and total
         var lines = $("table.line").find("tbody").find("tr").not("tr.empty-form");
         var goods = 0;
         var vat = 0;
@@ -40,12 +41,49 @@ $(document).ready(function () {
         goods = goods / 100;
         vat = vat / 100;
         total = total / 100;
+        // second calculate the totals for matched and due
+        var existing_matches = $("table.existing_matched_transactions").find("tbody").find("tr").not("tr.empty-form");
+        var new_matches = $("table.new_matched_transactions").find("tbody").find("tr").not("tr.empty-form");
+        var matched_total = 0;
+        existing_matches.each(function (index, tr) {
+            var elem = $(tr).find(":input").filter(function (index) {
+                return this.name.match(/match-\d+-value/);
+            });
+            matched_total = matched_total + ((+elem.val() || 0) * 100);
+        });
+        new_matches.each(function (index, tr) {
+            var elem = $(tr).find(":input").filter(function (index) {
+                return this.name.match(/match-\d+-value/);
+            });
+            matched_total = matched_total + ((+elem.val() || 0) * 100);
+        });
+        matched_total = matched_total / 100;
+        var due = total + matched_total;
+        var invalid_matching_error_class = undefined;
+        if((total >= 0 && due > total) || (total <= 0 && due < total)){
+            // invalid matching
+            invalid_matching_error_class = "bg-danger"
+        }
         goods = goods.toFixed(2);
         vat = vat.toFixed(2);
         total = total.toFixed(2);
-        $("div.sub-total-lines").text(goods);
-        $("div.vat-total-lines").text(vat);
-        $("div.total-lines").text(total);
+        $("td.goods-total-lines").text(goods);
+        $("td.vat-total-lines").text(vat);
+        $("td.total-lines").text(total);
+        matched_total = matched_total.toFixed(2);
+        due = due.toFixed(2)
+        var matched_total_elem = $("td.matched-total");
+        var due_total_elem = $("td.due-total");
+        matched_total_elem.text(matched_total);
+        due_total_elem.text(due);
+        // if(invalid_matching_error_class){
+        //     matched_total_elem.addClass(invalid_matching_error_class);
+        //     due_total_elem.addClass(invalid_matching_error_class);
+        // }
+        // else{
+        //     matched_total_elem.removeClass(invalid_matching_error_class);
+        //     due_total_elem.removeClass(invalid_matching_error_class);  
+        // }
     }
 
     $("table.line").on("change", ":input", function (event) {
@@ -77,6 +115,17 @@ $(document).ready(function () {
 
         calculate_totals();
 
+    });
+
+
+    $("table.existing_matched_transactions").on("change", "input", function (event) {
+        event.stopPropagation();
+        calculate_totals();
+    });
+
+    $("table.new_matched_transactions").on("change", "input", function (event) {
+        event.stopPropagation();
+        calculate_totals();
     });
 
     // on page load.  So either for editing, or errors returned when
