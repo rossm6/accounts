6b62be60bd0aae736c83a2559b84522776e02df0
Ross Meredith <rossm6@googlemail.com>
2020-09-13 17:37:40 +0100

    sales invoice view test complete

---
+++ sales/testing/test_transaction_views.py
@@ -0,0 +1,100 @@
+from datetime import datetime, timedelta
+from json import loads
+
+from django.shortcuts import reverse
+from django.test import RequestFactory, TestCase
+from django.utils import timezone
+
+from accountancy.testing.helpers import *
+from cashbook.models import CashBook, CashBookTransaction
+from items.models import Item
+from nominals.models import Nominal, NominalTransaction
+from utils.helpers import sort_multiple
+from vat.models import Vat
+
+from ..helpers import (create_credit_note_with_lines,
+                       create_credit_note_with_nom_entries,
+                       create_invoice_with_lines,
+                       create_invoice_with_nom_entries, create_invoices,
+                       create_lines, create_receipt_with_nom_entries,
+                       create_receipts, create_refund_with_nom_entries)
+from ..models import SaleHeader, SaleLine, SaleMatching, Customer
+
+HEADER_FORM_PREFIX = "header"
+LINE_FORM_PREFIX = "line"
+MATCHING_FORM_PREFIX = "match"
+PERIOD = '202007'  # the calendar month i made the change !
+SL_MODULE = "SL"
+
+
+class ViewInvoice(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+
+        cls.factory = RequestFactory()
+        cls.customer = Customer.objects.create(name="test_customer")
+        cls.ref = "test matching"
+        cls.date = datetime.now().strftime('%Y-%m-%d')
+        cls.due_date = (datetime.now() + timedelta(days=31)).strftime('%Y-%m-%d')
+
+        cls.item = Item.objects.create(code="aa", description="aa-aa")
+        cls.description = "a line description"
+
+        # ASSETS
+        assets = Nominal.objects.create(name="Assets")
+        current_assets = Nominal.objects.create(parent=assets, name="Current Assets")
+        cls.nominal = Nominal.objects.create(parent=current_assets, name="Bank Account")
+        cls.sale_control = Nominal.objects.create(
+            parent=current_assets, name="Sales Ledger Control"
+        )
+
+        # LIABILITIES
+        liabilities = Nominal.objects.create(name="Liabilities")
+        current_liabilities = Nominal.objects.create(parent=liabilities, name="Current Liabilities")
+        cls.vat_nominal = Nominal.objects.create(parent=current_liabilities, name="Vat")
+
+        cls.vat_code = Vat.objects.create(code="1", name="standard rate", rate=20)
+
+
+    def test(self):
+
+        create_invoice_with_nom_entries(
+            {
+                "type": "si",
+                "customer": self.customer,
+                "ref": self.ref,
+                "date": self.date,
+                "due_date": self.due_date,
+                "total": 2400,
+                "paid": 0,
+                "due": 2400
+            },
+            [
+                {
+                    'item': self.item,
+                    'description': self.description,
+                    'goods': 100,
+                    'nominal': self.nominal,
+                    'vat_code': self.vat_code,
+                    'vat': 20
+                }
+            ] * 20,
+            self.vat_nominal,
+            self.sale_control
+        )
+
+        headers = SaleHeader.objects.all()
+        header = headers[0]
+
+        response = self.client.get(
+            reverse("sales:view", kwargs={"pk": header.pk}))
+        self.assertEqual(
+            response.status_code,
+            200
+        )
+
+        self.assertEqual(
+            response.context["header_to_edit"],
+            header
+        )
