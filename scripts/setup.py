f605f67d6c2860f26357709a8dc50c95d296d806
ross <rossm6@googlemail.com>
2021-01-09 16:58:56 +0000

    change app.json

--- scripts/setup.py
+++ scripts/setup.py
@@ -9,29 +9,28 @@ from vat.models import Vat
 
 
 def run():
-    first = ModuleSettings.objects.first()
-    if not first:
-        ModuleSettings.objects.create()
-        # there should only ever be one record
 
     # A row should exist for each module from which you can post
-    QueuePosts.objects.get_or_create(module="c") # cashbook
-    QueuePosts.objects.get_or_create(module="n") # nominals
-    QueuePosts.objects.get_or_create(module="p") # purchases
-    QueuePosts.objects.get_or_create(module="s") # sales
+    QueuePosts.objects.get_or_create(module="c")  # cashbook
+    QueuePosts.objects.get_or_create(module="n")  # nominals
+    QueuePosts.objects.get_or_create(module="p")  # purchases
+    QueuePosts.objects.get_or_create(module="s")  # sales
 
     # revenue
     revenue = Nominal.objects.create(name="Revenue", type="pl")
     revenue = Nominal.objects.create(name="Revenue", type="pl", parent=revenue)
     # expenses
     expenses = Nominal.objects.create(name="Expenses", type="pl")
-    expenses = Nominal.objects.create(name="Expenses", type="pl", parent=expenses)
+    expenses = Nominal.objects.create(
+        name="Expenses", type="pl", parent=expenses)
     # assets
     assets = Nominal.objects.create(name="Assets", type="b")
     current_assets = Nominal.objects.create(
         name="Current Assets", parent=assets, type="b")
-    Nominal.objects.create(name="Sales Ledger Control", parent=current_assets, type="b")
-    bank_account = Nominal.objects.create(name="Bank Account", parent=current_assets, type="b")
+    Nominal.objects.create(name="Sales Ledger Control",
+                           parent=current_assets, type="b")
+    bank_account = Nominal.objects.create(
+        name="Bank Account", parent=current_assets, type="b")
     Nominal.objects.create(name="Prepayments", parent=current_assets, type="b")
     non_current_assets = Nominal.objects.create(
         name="Non Current Assets", parent=assets, type="b")
@@ -41,11 +40,13 @@ def run():
     current_liabilities = Nominal.objects.create(
         name="Current Liabilities", parent=liabilities, type="b")
     Nominal.objects.create(name="Purchase Ledger Control",
-                            parent=current_liabilities, type="b")
-    Nominal.objects.create(name="Vat Control", parent=current_liabilities, type="b")
+                           parent=current_liabilities, type="b")
+    Nominal.objects.create(
+        name="Vat Control", parent=current_liabilities, type="b")
     non_current_liabilities = Nominal.objects.create(
         name="Non Current Liabilities", parent=liabilities, type="b")
-    Nominal.objects.create(name="Loans", parent=non_current_liabilities, type="b")
+    Nominal.objects.create(
+        name="Loans", parent=non_current_liabilities, type="b")
     # equity
     equity = Nominal.objects.create(name="Equity", type="b")
     equity = Nominal.objects.create(name="Equity", type="b", parent=equity)
@@ -76,13 +77,21 @@ def run():
     ]
     Vat.objects.bulk_create(vat_rates)
 
-    fy = FinancialYear.objects.create(financial_year=2021, number_of_periods=12)
+    fy = FinancialYear.objects.create(
+        financial_year=2021, number_of_periods=12)
     periods = []
     for i in range(1, 13):
         p = str(i).rjust(2, "0")
-        p = Period(period=p, fy_and_period="2021" + p, month_start=date(2021,i, 1), fy=fy)
+        p = Period(period=p, fy_and_period="2021" + p,
+                   month_start=date(2021, i, 1), fy=fy)
         periods.append(p)
     Period.objects.bulk_create(periods)
+
+    first_period = Period.objects.first()
+
+    ModuleSettings.objects.create(cash_book_period=first_period, nominals_period=first_period,
+                                  purchases_period=first_period, sales_period=first_period)
+    # there should only ever be one record
 
     Customer.objects.create(name="Hilton", code="hilton", customer=True)
     Supplier.objects.create(name="Screwfix", code="screw", supplier=True)
